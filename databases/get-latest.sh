31be800a7e59eeaa30493d3638e675a80824bc8b
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-10-09 11:52:40 -0700

    latest

--- databases/get-latest.sh
+++ databases/get-latest.sh
@@ -1,2 +1,38 @@
-\copy series_stats to 'series_stats.csv' CSV HEADER
+## Export soil series databases from SoilWeb
+## D.E. Beaudette
+## 2018-10-09
+##
+## notes:
+##    - SC database is re-made elsewhere (dump-NASIS-SC-data.R)
+##    - 
+##
+
+
+
+## cleanup old versions
+
+# geomorph / parent material
+rm geomcomp_wide.csv.gz hillpos_wide.csv.gz mountainpos_wide.csv.gz
+
+# series / other taxa stats
+rm series_stats.csv.gz taxsubgrp-stats.txt
+
+# parent material
+rm pmkind.csv.gz pmorigin.csv.gz 
+
+# MLRA membership
+rm series-mlra-overlap.csv.gz
+
+
+## manually export latest versions
+# db
+# user
+sh export-tables.sh
+
+## compute subgroup stats ~ 1 hour
+bash soilweb-acreage-by-subgroup.sh
+
+## gzip for github
+gzip *.csv
+
 
