f00414b773e622f35d53f9a9069611f014afc2be
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-04-05 16:12:12 -0700

    new examples c/o ropensci crowd

--- taxa-library-example.R
+++ taxa-library-example.R
@@ -11,6 +11,30 @@ data("ST", package = 'SoilTaxonomy')
 # convert to Taxmap class
 x <- parse_tax_data(ST, class_cols = names(ST), named_by_rank = TRUE)
 
+
+x %>% 
+  filter_taxa(taxon_names == "alfisols", subtaxa = TRUE) %>%
+  heat_tree(node_label = taxon_names,
+            node_size = n_obs, 
+            node_color = n_obs,
+            node_color_axis_label = "Number of subgroups",
+            output_file = "alfisols_tree.pdf")
+
+
+set.seed(4)
+x %>% 
+  filter_taxa(!is_leaf) %>%
+  heat_tree(node_label = taxon_names,
+            node_size = n_obs, 
+            node_color = n_obs,
+            tree_label = taxon_names,
+            node_color_axis_label = "Number of subgroups",
+            layout = "da",
+            output_file = "soil_tree.pdf")
+
+
+
+
 # what does this do?
 # https://grunwaldlab.github.io/metacoder_documentation/example.html
 heat_tree(x)
