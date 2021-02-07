f272c87ebd3fa1a6aca02508ba0cad0b7f04afd4
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-02-06 19:47:47 -0800

    getParentTaxa: better example

--- R/getParentTaxa.R
+++ R/getParentTaxa.R
@@ -9,15 +9,19 @@
 #' @param convert Convert results from taxon codes to taxon names? Default: `TRUE`
 #'
 #' @return A named list, where names are taxon codes and values are character vectors representing parent taxa
+#' @author Andrew G. Brown
 #' @export
 #'
 #' @examples
 #' 
 #' getParentTaxa("ultic haploxeralfs")
 #' 
-#' getParentTaxa("folists")
+#' getParentTaxa(code = c("ABCD", "DABC"))
+#' 
+#' getParentTaxa("folists", convert = FALSE)
 #' 
 getParentTaxa <- function(taxon = NULL, code = NULL, convert = TRUE) {
+  
   # take taxon names or codes as input
   stopifnot(!is.null(taxon) | !is.null(code))
   
@@ -31,14 +35,14 @@ getParentTaxa <- function(taxon = NULL, code = NULL, convert = TRUE) {
   
   # iterate over list - 1 element/taxon each with composite codes
   lapply(dtc, function(x) {
-      if(all(is.na(x)))
+      if (all(is.na(x)))
         return(NA)
     
       # take all codes except last (self) code
       y <- do.call('c', x[1:(length(x) - 1)])
       
       # convert code to taxon if needed
-      if(convert) 
+      if (convert) 
         return(taxon_code_to_taxon(y))
       
       return(y)
