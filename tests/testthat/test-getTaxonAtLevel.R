cd045bd68a3ec8293f5572ed07ae5260799b160b
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-02-18 13:28:32 -0800

    getTaxonAtLevel: complete test coverage

--- tests/testthat/test-getTaxonAtLevel.R
+++ tests/testthat/test-getTaxonAtLevel.R
@@ -1,12 +1,27 @@
 test_that("getTaxonAtLevel works", {
 
   # specify alternate levels
-  expect_equal(as.character(getTaxonAtLevel("humic haploxerands", level = "greatgroup")), "haploxerands")
+  expect_equal(getTaxonAtLevel("humic haploxerands", level = "greatgroup"), 
+               c(DECH = "haploxerands"))
 
   # can't get subgroup (child) from suborder (parent), can get suborder from suborder
   expect_equal(getTaxonAtLevel(c('folists','plinthic kandiudults'), level = "subgroup"),
                c(BA = NA, HCCN = "plinthic kandiudults"))
 
   # but can do parents of children
-  expect_equal(as.character(getTaxonAtLevel("udifolists", level = "suborder")), "folists")
+  expect_equal(getTaxonAtLevel("udifolists", level = "suborder"), 
+               c(BAD = "folists"))
+  
+  # default is level="soilorder"
+  expect_equal(getTaxonAtLevel("udolls"), 
+               c(IH = "mollisols"))
+  
+  # garbage and 0-character input
+  g1 <- getTaxonAtLevel("foo") 
+  g2 <- getTaxonAtLevel("") # 
+  expect_true(is.na(g1[[1]]) && is.na(g2[[1]]) && 
+                is.na(names(g1)[1]) && is.na(names(g2)[1]))
+  
+  # empty (length 0) and missing (NA) input
+  expect_true(is.na(getTaxonAtLevel(character(0))) && is.na(getTaxonAtLevel(NA)))
 })
