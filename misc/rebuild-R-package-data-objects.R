c81372c97f2f89cdcdf75e6640102133f8705abe
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-04-13 09:07:24 -0700

    Incorporate Ch2 definitions into features dataset RE: https://github.com/ncss-tech/SoilKnowledgeBase/pull/21

--- misc/rebuild-R-package-data-objects.R
+++ misc/rebuild-R-package-data-objects.R
@@ -28,40 +28,42 @@ save(ST_formative_elements, file='data/ST_formative_elements.rda')
 
 ST_feature_SKB <- jsonlite::read_json("https://github.com/ncss-tech/SoilKnowledgeBase/raw/main/inst/extdata/KST/2014_KST_EN_featurelist.json",
                                    simplifyVector = TRUE)
+# types of soil materials and their differentia
+materials <- ST_feature_SKB[1:7,]
 
 # there are headers and some explanatory sections in this chapter
-epipedons <- ST_feature_SKB[2:9,]
+epipedons <- ST_feature_SKB[9:16,]
 # c("Anthropic Epipedon", "Folistic Epipedon", "Histic Epipedon", 
 #   "Melanic Epipedon", "Mollic Epipedon", "Ochric Epipedon", "Plaggen Epipedon", 
 #   "Umbric Epipedon")
 
-diagnostic_horizons <- ST_feature_SKB[11:30,]
+diagnostic_horizons <- ST_feature_SKB[18:37,]
 # c("Agric Horizon", "Albic Horizon", "Anhydritic Horizon", "Argillic Horizon", 
 #   "Calcic Horizon", "Cambic Horizon", "Duripan", "Fragipan", "Glossic Horizon", 
 #   "Gypsic Horizon", "Kandic Horizon", "Natric Horizon", "Ortstein", 
 #   "Oxic Horizon", "Petrocalcic Horizon", "Petrogypsic Horizon", 
 #   "Placic Horizon", "Salic Horizon", "Sombric Horizon", "Spodic Horizon")
 
-characteristics_mineral <- ST_feature_SKB[32:41,]
+characteristics_mineral <- ST_feature_SKB[39:48,]
 # c("Abrupt Textural Change", "Albic Materials", "Andic Soil Properties", 
 #   "Anhydrous Conditions", "Coefficient of Linear Extensibility (COLE)", 
 #   "Fragic Soil Properties", "Free Carbonates", "Identifiable Secondary Carbonates", 
 #   "Interfingering of Albic Materials", "Lamellae")
 
-characteristics_organic <- ST_feature_SKB[53:66,]
+characteristics_organic <- ST_feature_SKB[60:73,]
 # c("Kinds of Organic Soil Materials", "Fibers", "Fibric Soil Materials", 
 #   "Hemic Soil Materials", "Sapric Soil Materials", "Humilluvic Material", 
 #   "Kinds of Limnic Materials", "Coprogenous Earth", "Diatomaceous Earth", 
 #   "Marl", "Thickness of Organic Soil Materials", "Surface Tier", 
 #   "Subsurface Tier", "Bottom Tier")
 
-characteristics_all <- ST_feature_SKB[68:81,]
+characteristics_all <- ST_feature_SKB[75:88,]
 # c("Aquic Conditions", "Cryoturbation", "Densic Contact", "Densic Materials", 
 #   "Gelic Materials", "Glacic Layer", "Lithic Contact", "Paralithic Contact", 
 #   "Permafrost", "Soil Moisture Regimes", "Soil Moisture Control Section", 
 #   "Classes of Soil Moisture Regimes", "Sulfidic Materials", "Sulfuric Horizon")
 
-characteristics_anthro <- ST_feature_SKB[83:93,]
+characteristics_anthro <- ST_feature_SKB[90:100,]
 # c("Anthropogenic Landforms", "Constructional Anthropogenic Landforms", 
 #   "Destructional Anthropogenic Landforms", "Anthropogenic Microfeatures", 
 #   "Constructional Anthropogenic Microfeatures", "Destructional Anthropogenic Microfeatures", 
@@ -69,12 +71,15 @@ characteristics_anthro <- ST_feature_SKB[83:93,]
 #   "Manufactured Layer", "Manufactured Layer Contact")
 
 ST_features <- list(
+  materials = materials,
   epipedons = epipedons,
   diagnostic_horizons = diagnostic_horizons,
   characteristics_mineral = characteristics_mineral,
   characteristics_organic = characteristics_organic,
-  characteristics_all = characteristics_all
+  characteristics_all = characteristics_all,
+  characteristics_anthro = characteristics_anthro
 )
-ST_features <- do.call('rbind',ST_features)
+
+ST_features <- do.call('rbind', ST_features)
 rownames(ST_features) <- NULL
 save(ST_features, file='data/ST_features.rda')
