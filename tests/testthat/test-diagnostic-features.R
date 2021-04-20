d10ad0a4d43d29331db8f010ec325d5c3e6f4580
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-04-19 19:59:53 -0700

    get_ST_features: Add tests for filtering

--- tests/testthat/test-diagnostic-features.R
+++ tests/testthat/test-diagnostic-features.R
@@ -13,4 +13,17 @@ test_that("get_ST_features works", {
   # basic test of contents, mollic epipedon is in there
   expect_true("Mollic Epipedon" %in% res$name)
   
+  # filtering using chapter and page
+  res <- get_ST_features(chapter = 2)
+  expect_equal(nrow(res), 7)
+  
+  res <- get_ST_features(page = 18:19)
+  expect_equal(nrow(res), 6)
+  
+  # filter on multiple [redundant] criteria
+  expect_equal(
+    get_ST_features(group = "Surface", name = "Mollic Epipedon"),
+    get_ST_features(name = "Mollic Epipedon")
+  )
+  
 })
