f0dd96503b14ec39c86cb5666c484522d3a5b555
Andrew Gene Brown <andrew.g.brown@usda.gov>
2022-07-11 13:52:20 -0700

    Extensions to `parse_family()` (#36)
    * Revert "(temporarily) roll back changes to parseFamily"
    
    This reverts commit f8b306fdc8a145bdc9764158dc4141dba8ed3fbe.
    
    * Revert "parse_family: `column_metadata` `FALSE` by default"
    
    This reverts commit 63d30a1e39ed151d9a35ba6ee08cb9d8ab72f7ec.
    
    * Safe vignette
    
    * Add data.table back to Imports
    
    * NAMESPACE/examples
    
    * Cleanup
    
    * Relax soilDB function def requirement for backward compatibility
    
    * Add reverted R-CMD-check for devel aqp/soilDB back in
    
    * Fix R<4.0 errors?
    
    * typo
    
    * Docs
    
    * Fix logic + docs
    
    * run example
    
    * dont skip
    
    * Missed one?

--- R/parseFamily.R
+++ R/parseFamily.R
@@ -1,27 +1,27 @@
-#' Parse components of a family-level taxon name
+#' Parse Components of a "Family-level" Taxon Name
 #'
 #' @param family a character vector containing taxonomic families, e.g. `"fine-loamy, mixed, semiactive, mesic ultic haploxeralfs"`
-#' @param column_metadata include parsed NASIS physical column names and values from family taxon components? Default: `FALSE`. `TRUE` requires soilDB package.
+#' @param column_metadata include parsed NASIS physical column names and values from family taxon components? Default: `TRUE` requires soilDB package.
 #'
 #' @return a `data.frame` containing column names: `"family"` (input), `"subgroup"` (parsed taxonomic subgroup), `"subgroup_code"` (letter code for subgroup), `"class_string"` (comma-separated family classes), `"classes_split"` (split class_string vector stored as `list` column).
 #'
-# In addition, the following column names are identified and returned based on NASIS (National Soil Information System) metadata (via soilDB package):
-#  - `"taxpartsize"`, `"taxpartsizemod"`, `"taxminalogy"`, `"taxceactcl"`, `"taxreaction"`, `"taxtempcl"`, `"taxsubgroup"`, `"taxgreatgroup"`, `"taxsuborder"`, `"taxorder"`
+#' In addition, the following column names are identified and returned based on NASIS (National Soil Information System) metadata (via soilDB package):
+#'  - `"taxpartsize"`, `"taxpartsizemod"`, `"taxminalogy"`, `"taxceactcl"`, `"taxreaction"`, `"taxtempcl"`, `"taxsubgroup"`, `"taxgreatgroup"`, `"taxsuborder"`, `"taxorder"`
 #'
 #' @export
 #'
 #' @examples
+#' if (!requireNamespace('soilDB')) {
+#'   families <- c("fine, kaolinitic, thermic typic kanhapludults",
+#'                 "fine-loamy, mixed, semiactive, mesic ultic haploxeralfs",
+#'                 "euic, thermic typic haplosaprists",
+#'                 "coarse-loamy, mixed, active, mesic aquic dystrudepts")
 #'
-#' families <- c("fine, kaolinitic, thermic typic kanhapludults",
-#'               "fine-loamy, mixed, semiactive, mesic ultic haploxeralfs",
-#'               "euic, thermic typic haplosaprists",
-#'               "coarse-loamy, mixed, active, mesic aquic dystrudepts")
-#'
-#' # inspect parsed list result
-#' str(parse_family(families))
-#'
+#'   # inspect parsed list result
+#'   str(parse_family(families))
+#' }
 #' @importFrom stringr str_locate
-parse_family <- function(family, column_metadata = FALSE) {
+parse_family <- function(family, column_metadata = TRUE) {
 
   # for R CMD check
   ST_unique_list <- NULL
@@ -49,60 +49,72 @@ parse_family <- function(family, column_metadata = FALSE) {
     class_string = family_classes,
     classes_split = I(lapply(strsplit(family_classes, ","), trimws)))
 
-  # if (!column_metadata)
+  if (!column_metadata)
     return(res)
-  # cbind(res, .get_family_differentia(res))
+  cbind(res, .get_family_differentia(res))
 }
 
-# @import data.table
-# @importFrom utils packageVersion
-# .get_family_differentia <- function(res) {
-#
-#   if (!requireNamespace("soilDB") || utils::packageVersion("soilDB") < "2.7.3") {
-#     stop("package `soilDB` >=2.7.3 is required to lookup NASIS column metadata correponding to taxonomic classes", call. = FALSE)
-#   } else {
-#     metadata <- soilDB::get_NASIS_metadata()
-#   }
-#
-#   ST_family_classes <- NULL
-#   load(system.file("data/ST_family_classes.rda", package = "SoilTaxonomy")[1])
-#
-#   # lookup classes in KST definitions
-#   kst_lookup <- lapply(res$classes_split, function(x) {
-#     ST_family_classes[match(tolower(x), tolower(ST_family_classes$classname)), c("classname", "group", "name")]
-#   })
-#
-#   # lookup classname -> (possible) NASIS domain ID
-#   nasis_family_classes <- metadata[c("ChoiceName", "DomainID")]
-#   colnames(nasis_family_classes) <- c("classname", "DomainID")
-#
-#   # combine KST and NASIS LUT
-#   res2 <- lapply(kst_lookup, function(x) {
-#     res3 <- merge(x, nasis_family_classes[nasis_family_classes$classname %in% x$classname, ],
-#           by = "classname", all.x = TRUE, all.y = TRUE, sort = FALSE)
-#     res3
-#   })
-#
-#   taxsub <- as.data.frame(do.call('rbind', lapply(decompose_taxon_code(res$subgroup_code), function(x) taxon_code_to_taxon(as.character(rev(x))))))
-#   colnames(taxsub) <- rev(c("taxorder", "taxsuborder", "taxgrtgroup", "taxsubgrp"))
-#   rownames(taxsub) <- NULL
-#
-#   res4 <- lapply(seq_along(res2), function(i) {
-#     x <- res2[[i]]
-#     md <- soilDB::get_NASIS_column_metadata(x$DomainID, "DomainID")
-#
-#     # omitting the "duplicate" "osdtax..." columns within single domain
-#     # also taxtempcl used over taxtempregime (which has identical levels)
-#     md <- md[!grepl("^osdtax|taxtempregime", md$ColumnPhysicalName),]
-#
-#     res5 <- unique(data.frame(
-#       classname = x$classname,
-#       ColumnPhysicalName = md$ColumnPhysicalName[match(x$classname, md$ChoiceName)]
-#     ))
-#     cbind(`colnames<-`(as.data.frame(t(res5[[1]])), res5[[2]]), taxsub[i, ])
-#   })
-#
-#   basetbl <- as.data.frame(`names<-`(rep(list(numeric(0)), 10), c("taxpartsize", "taxpartsizemod", "taxminalogy", "taxceactcl", "taxreaction", "taxtempcl", "taxsubgroup", "taxgreatgroup",
-#                                                                   "taxsuborder", "taxorder")))
-#   as.data.frame(data.table::rbindlist(c(list(basetbl), res4), fill = TRUE))
-# }
+#' @import data.table
+.get_family_differentia <- function(res) {
+
+  metadata <- NULL
+  if (!requireNamespace("soilDB")) {
+    message("package `soilDB` is required to lookup NASIS column metadata correponding to taxonomic classes", call. = FALSE)
+    return(res[, 0, drop = FALSE])
+  } else {
+    # soilDB::get_NASIS_metadata() # 2.7.3+
+    load(system.file("data/metadata.rda", package = "soilDB")[1])
+  }
+
+  ST_family_classes <- NULL
+  load(system.file("data/ST_family_classes.rda", package = "SoilTaxonomy")[1])
+
+  # lookup classes in KST definitions
+  kst_lookup <- lapply(res$classes_split, function(x) {
+    ST_family_classes[match(tolower(x), tolower(ST_family_classes$classname)), c("classname", "group", "name")]
+  })
+
+  # lookup classname -> (possible) NASIS domain ID
+  nasis_family_classes <- metadata[c("ChoiceName", "DomainID")]
+  colnames(nasis_family_classes) <- c("classname", "DomainID")
+
+  # combine KST and NASIS LUT
+  res2 <- lapply(kst_lookup, function(x) {
+    res3 <- merge(x, nasis_family_classes[nasis_family_classes$classname %in% x$classname, ],
+          by = "classname", all.x = TRUE, all.y = TRUE, sort = FALSE)
+    res3
+  })
+
+  taxsub <- as.data.frame(do.call('rbind', lapply(decompose_taxon_code(res$subgroup_code), function(x) taxon_code_to_taxon(as.character(rev(x))))),
+                          stringsAsFactors = FALSE)
+  colnames(taxsub) <- rev(c("taxorder", "taxsuborder", "taxgrtgroup", "taxsubgrp"))
+  rownames(taxsub) <- NULL
+
+  res4 <- lapply(seq_along(res2), function(i) {
+    x <- res2[[i]]
+    # md <- soilDB::get_NASIS_column_metadata(x$DomainID, "DomainID")
+    md <- metadata[metadata$DomainID %in% x$DomainID, ]
+
+    # omitting the "duplicate" "osdtax..." columns within single domain
+    # also taxtempcl used over taxtempregime (which has identical levels)
+    md <- md[!grepl("^osdtax|taxtempregime", md$ColumnPhysicalName),]
+
+    res5 <- unique(data.frame(
+      classname = x$classname,
+      ColumnPhysicalName = md$ColumnPhysicalName[match(x$classname, md$ChoiceName)],
+      stringsAsFactors = FALSE
+    ))
+    cbind(`colnames<-`(as.data.frame(t(res5[[1]]), stringsAsFactors = FALSE), res5[[2]]), taxsub[i, ])
+  })
+
+  basetbl <- as.data.frame(`names<-`(rep(list(numeric(0)), 10), c("taxpartsize", "taxpartsizemod",
+                                                                  "taxminalogy", "taxceactcl",
+                                                                  "taxreaction", "taxtempcl",
+                                                                  "taxsubgroup", "taxgreatgroup",
+                                                                  "taxsuborder", "taxorder")),
+                           stringsAsFactors = FALSE)
+  as.data.frame(data.table::rbindlist(c(list(basetbl), res4), fill = TRUE),
+                stringsAsFactors = FALSE)
+}
+
+
