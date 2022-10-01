0629ed799267ca8e82c9efac80c9aac4fbca3b19
Andrew Gene Brown <andrew.g.brown@usda.gov>
2022-10-01 16:40:25 -0700

    Add `SoilTaxonomyLevels()` (#39)
    * Add `SoilTaxonomyLevels()`
     - get ordinal or nominal factors with taxonomic groups as labels
    
    * Check `level` arg against available choices
    
    * remove comment
    
    * Doc/examples

---
+++ R/SoilTaxonomyLevels.R
@@ -0,0 +1,33 @@
+#' Get (Ordered) Factors based on Soil Taxonomy Key position
+#'
+#' @param level One of: `"order"`, `"suborder"`, `"greatgroup"`, `"subgroup"`
+#' @param as.is Return character labels rather than an (ordered) factor? Default: `FALSE`
+#' @param ordered Create an ordinal factor? Default: `TRUE`
+#'
+#' @return an (ordered) factor or character vector (when `as.is=TRUE`)
+#' @export
+#'
+#' @examples
+#'
+#' SoilTaxonomyLevels("order")
+#'
+#' SoilTaxonomyLevels("order", ordered = FALSE)
+#'
+#' SoilTaxonomyLevels("order", as.is = TRUE)
+#'
+#' SoilTaxonomyLevels("suborder")
+#'
+SoilTaxonomyLevels <- function(level = c("order", "suborder", "greatgroup", "subgroup"),
+                               as.is = FALSE, ordered = TRUE) {
+
+  level <- match.arg(level, c("order", "suborder", "greatgroup", "subgroup"))
+  ST_unique_list <- NULL
+  load(system.file("data/ST_unique_list.rda", package = "SoilTaxonomy")[1])
+
+  res <- factor(tolower(ST_unique_list[[level]]), levels = ST_unique_list[[level]], ordered = ordered)
+
+  if (as.is) {
+    return(as.character(res))
+  }
+  res
+}
