6f9b4a05c7e5b39e705caf0730cbf27ccc9f467b
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-03-09 08:48:34 -0800

    taxonTree: cleanup and add customizable `level` argument

--- R/taxonTree.R
+++ R/taxonTree.R
@@ -2,33 +2,45 @@
 #' Create a `data.tree` Object from Taxon Names
 #'
 #' @param taxon A vector of taxon names
+#' @param level One or more of: `"order"`, `"suborder"`, `"greatgroup"`, `"subgroup"`. The lowest level is passed to `getChildLevel()` to generate the leaf nodes.
 #' @param root Label for root node. Default: `"Soil Taxonomy"`; `NULL` for "unrooted" tree.
 #' @param verbose Print tree output? Default: `TRUE`
 #' @param ... Additional arguments to `data.tree::as.Node.data.frame()`
 #'
-#' @return A data.tree object (invisibly). A text representation of the tree is printed when `verbose=TRUE`.
+#' @return A `data.tree` object (invisibly). A text representation of the tree is printed when `verbose=TRUE`.
 #' @export
+#' @importFrom stats complete.cases
 #' @examplesIf !inherits(requireNamespace("data.tree", quietly = TRUE), 'try-error')
 #' @examples
+#'
+#' # hapludults and hapludalfs (to subgroup level)
 #' taxonTree(c("hapludults", "hapludalfs"))
-taxonTree <- function(taxon, root = "Soil Taxonomy", verbose = TRUE, ...) {
+#'
+#' # alfisols suborders and great groups
+#' taxonTree("alfisols", root = "Alfisols", level = c("suborder", "greatgroup"))
+taxonTree <- function(taxon,
+                      level = c("order", "suborder", "greatgroup", "subgroup"),
+                      root = "Soil Taxonomy",
+                      verbose = TRUE,
+                      ...) {
   if (!requireNamespace("data.tree")) {
     stop("package 'data.tree' is required", call. = FALSE)
   }
 
-  x <- unique(do.call('c', getChildTaxa(taxon)))
-  y <- getTaxonAtLevel(x, level = c("order", "suborder", "greatgroup", "subgroup"))
+  level <- tolower(trimws(level))
+  lowest_level <- max(match(level, level_hierarchy(family = FALSE)))
+  x <- unique(do.call('c', getChildTaxa(taxon, level = level[length(level)])))
+  y <- getTaxonAtLevel(x, level = level)
   y <- y[order(taxon_to_taxon_code(x)),]
-  y <- y[complete.cases(y),]
+  y <- y[stats::complete.cases(y),]
 
-  y$pathString <- with(y, paste0(ifelse(length(root) > 0, paste0(root, "/"), ""),
-                                 order, "/", suborder, "/", greatgroup, "/", subgroup))
+  y$pathString <- apply(data.frame(root, as.data.frame(lapply(level, function(z) {
+    paste0("/", y[[z]])
+  }))), MARGIN = 1, FUN = paste0, collapse = "")
+
   n <- data.tree::as.Node(y, ...)
   if (isTRUE(verbose)) {
     print(n, limit = NULL)
   }
-
   invisible(n)
 }
-
-taxonTree("alfisols")
