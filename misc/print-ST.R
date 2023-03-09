1e49d58b145d8020c29c2707007c3e7bf446ab7b
Beaudette <dylan.beaudette@usda.gov>
2023-03-08 23:51:01 -0800

    new take on printing ST, still very rough

---
+++ misc/print-ST.R
@@ -0,0 +1,94 @@
+library(fs)
+library(SoilTaxonomy)
+library(soilDB)
+library(data.tree)
+
+
+## 2023-03-08: new take on ST-tree.R code from many years ago
+
+## TODO:
+# * add subgroup acreage
+# * 13th ed.
+# * compact style of dir_tree(), but in proper order
+
+data("ST")
+
+# just in case
+x <- ST[order(ST$code), ]
+
+sc <- get_soilseries_from_NASIS()
+
+sc <- sc[, c('soilseriesname', 'taxclname', 'taxsubgrp')]
+names(sc) <- c('series', 'family', 'subgroup')
+
+z <- merge(x, sc, by.x = 'subgroup', all.x = TRUE, sort = FALSE)
+head(z)
+
+z$family <- tolower(z$family)
+z$series <- tolower(z$series)
+
+z$f <- NA_character_
+for(i in 1:nrow(z)) {
+  z$f[i] <- gsub(pattern = z$subgroup[i], replacement = '', z$family[i], fixed = TRUE)
+}
+
+z$f <- trimws(z$f, which = 'both')
+
+head(z)
+
+z <- z[, c('order', 'suborder', 'greatgroup', 'subgroup', 'f', 'series')]
+
+head(z)
+
+
+
+td <- tempdir()
+unlink(file.path(td, 'ST'), recursive = TRUE)
+
+for(i in 1:nrow(x)) {
+  dir.create(
+    file.path(
+      td, 
+      'ST', 
+      z$order[i], 
+      z$suborder[i], 
+      z$greatgroup[i], 
+      z$subgroup[i], 
+      z$f[i], 
+      z$series[i]
+    ), 
+    recursive = TRUE
+  )
+}
+
+setwd(td)
+
+sink('e:/temp/st12.txt')
+dir_tree(file.path('ST'))
+sink()
+
+unlink(file.path(td, 'ST'), recursive = TRUE)
+
+
+z$path <- sprintf("ST/%s/%s/%s/%s/%s/%s", z$order, z$suborder, z$greatgroup, z$subgroup, z$f, z$series)
+n <- as.Node(z, pathName = 'path')
+
+
+# prune missing family / series
+pf <- function(i) {
+  
+  if(GetAttribute(i, 'name') == 'NA') {
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
+  
+}
+
+options('max.print' = 1e7)
+sink('e:/temp/st12-DT.txt')
+print(n, limit = NULL, pruneFun = pf)
+sink()
+
+options('max.print' = 1000)
+
