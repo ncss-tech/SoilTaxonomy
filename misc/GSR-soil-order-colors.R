4bcceb6686bf3b5eadc058725268a394c1dd9c80
Beaudette <dylan.beaudette@usda.gov>
2023-03-14 11:57:50 -0700

    maybe useful for standardization of maps / thematic representation

---
+++ misc/GSR-soil-order-colors.R
@@ -0,0 +1,41 @@
+## colors used in Global Soil Regions maps
+## c/o Paul Reich
+
+
+colors2022 <- structure(
+  list(
+    Order = c("Alfisols", "Andisols", "Aridisols", 
+              "Entisols", "Gelisols", "Histosols", "Inceptisols", "Mollisols", 
+              "Oxisols", "Spodosols", "Ultisols", "Vertisols", "Shifting Sand", 
+              "Rock"), 
+    R = c(85L, 255L, 255L, 255L, 102L, 137L, 215L, 38L, 
+          255L, 194L, 255L, 0L, 204L, 130L), 
+    G = c(255L, 0L, 235L, 170L, 
+          119L, 90L, 215L, 115L, 0L, 158L, 255L, 77L, 204L, 130L), 
+    B = c(0L, 
+          197L, 175L, 0L, 205L, 68L, 158L, 0L, 0L, 215L, 0L, 168L, 204L, 
+          130L)
+  ), 
+  class = "data.frame", 
+  row.names = c(NA, -14L)
+)
+
+
+
+colors2006 <- structure(
+  list(
+    Order = c("Alfisols", "Andisols", "Aridisols", 
+              "Entisols", "Gelisols", "Histosols", "Inceptisols", "Mollisols", 
+              "Oxisols", "Spodosols", "Ultisols", "Vertisols", "Shifting Sand", 
+              "Rock"), 
+    R = c(102L, 197L, 255L, 255L, 122L, 137L, 217L, 0L, 
+          227L, 194L, 255L, 0L, 204L, 130L), 
+    G = c(189L, 0L, 235L, 145L, 
+          182L, 68L, 240L, 104L, 28L, 158L, 255L, 92L, 204L, 130L), 
+    B = c(99L, 
+          255L, 190L, 71L, 245L, 68L, 163L, 55L, 28L, 215L, 51L, 230L, 
+          204L, 130L)
+  ), 
+  class = "data.frame", 
+  row.names = c(NA, -14L)
+)
